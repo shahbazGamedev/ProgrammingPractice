cfb03e7fa8d8fdae7ec40126133f75544ed306e8
toshkuuu <todorr@live.com>
2015-04-29 15:36:33 +0300

    file changes

--- Homework5/Problem7_SubsetSumToZero/SumToZero.cs
+++ Homework5/Problem7_SubsetSumToZero/SumToZero.cs
@@ -7,7 +7,7 @@ using System.Collections.Generic;
 
 class SumToZero
 {
-     public static void Main()
+    public static void Main()
     {
         // this algorithm uses a recursive method to calculate if the the current sum of numbers equals the desired one(0)
         // the main list is beign split until it restores its original size
@@ -15,7 +15,7 @@ class SumToZero
         // the programs stops when the original list size counter (countOfNumbers) reaches zero
 
         List<int> numbersList = new List<int>() { 5, 0, -2, -3, 1 };
-        int targetSum = 0;
+        int targetSum = 1;
         int countOfNumbers = numbersList.Count;
 
         DoSumRecursive(countOfNumbers, targetSum, numbersList, new List<int>());
@@ -23,49 +23,46 @@ class SumToZero
         Console.WriteLine();
     }
 
-     private static void DoSumRecursive(int countOfNumbers, int targetSum, List<int> numbersList, List<int> partialList)
-     {
-         // check if the program should end: 
-         if (countOfNumbers == 0)
-         {
-             return;
-         }
-         else
-         {
-             countOfNumbers--;
-         }
+    private static void DoSumRecursive(int countOfNumbers, int targetSum, List<int> numbersList, List<int> partialList)
+    {
+        // check if the program should end: 
+        if (countOfNumbers == 0)
+        {
+            return;
+        }
+        else
+        {
+            countOfNumbers--;
+        }
 
-         // set a value to the new sum:
-         int newSum = 0;
-         foreach (int number in partialList)
-         {
-             newSum += number;
-         }
+        // set a value to the new sum:
+        int newSum = 0;
+        foreach (int number in partialList)
+        {
+            newSum += number;
+        }
 
-         // print the new sum if it equals the desired one:
-         if (newSum == targetSum && partialList.Count != 0)
-         {
-             Console.WriteLine(string.Join(" + ", partialList.ToArray()) + " = " + targetSum);
-         }
+        // print the new sum if it equals the desired one:
+        if (newSum == targetSum && partialList.Count != 0)
+        {
+            Console.WriteLine(string.Join(" + ", partialList.ToArray()) + " = " + targetSum);
+        }
 
-         // make changes to the other lists:
-         for (int i = 0; i < numbersList.Count; i++)
-         {
-             // used for storing the remaining numbers from the original list of numbers (numbersList):
-             List<int> remainingNumbersList = new List<int>();
-             for (int j = i + 1; j < numbersList.Count; j++)
-             {
-                 remainingNumbersList.Add(numbersList[j]);
-             }
+        // make changes to the other lists:
+        for (int i = 0; i < numbersList.Count; i++)
+        {
+            // used for storing the remaining numbers from the original list of numbers (numbersList):
+            List<int> remainingNumbersList = new List<int>();
+            for (int j = i + 1; j < numbersList.Count; j++)
+            {
+                remainingNumbersList.Add(numbersList[j]);
+            }
 
-             // represents the original list of numbers (numbersList):
-             List<int> partialRecursiveList = new List<int>(partialList);
-             partialRecursiveList.Add(numbersList[i]);
+            // represents the original list of numbers (numbersList):
+            List<int> partialRecursiveList = new List<int>(partialList);
+            partialRecursiveList.Add(numbersList[i]);
 
-             DoSumRecursive(countOfNumbers, targetSum, remainingNumbersList, partialRecursiveList);
-         }
-     }
+            DoSumRecursive(countOfNumbers, targetSum, remainingNumbersList, partialRecursiveList);
+        }
+    }
 }
-
-
-
