abe357c0a722396a8b524c1bb48c21d7984c4030
toshkuuu <todorr@live.com>
2015-06-26 13:31:36 +0300

    changes

---
+++ Module2/3.DefiningClassesPart2/2.MathMatrix/Libs/Matrix.cs
@@ -0,0 +1,135 @@
+﻿namespace MathMatrix.Libs
+{
+    using System;
+    using System.Text;
+
+    public class Matrix
+    {
+        public Matrix(int rows, int cols)
+        {
+            this.Elements = new int[rows, cols];
+        }
+
+        public int[,] Elements { get; set; }
+
+        public static Matrix operator +(Matrix first, Matrix second)
+        {
+            if (!first.Equals(second))
+            {
+                throw new ArgumentOutOfRangeException("Matrices should be of equal sizes in order to be added.");
+            }
+
+            Matrix result = new Matrix(first.Elements.GetLength(0), first.Elements.GetLength(1));
+            for (int i = 0; i < first.Elements.GetLength(0); i++)
+            {
+                for (int j = 0; j < first.Elements.GetLength(1); j++)
+                {
+                    result.Elements[i, j] = first.Elements[i, j] + second.Elements[i, j];
+                }
+            }
+
+            return result;
+        }
+
+        public static Matrix operator -(Matrix first, Matrix second)
+        {
+            if (!first.Equals(second))
+            {
+                throw new ArgumentOutOfRangeException("Matrices should be of equal sizes in order to be added.");
+            }
+
+            Matrix result = new Matrix(first.Elements.GetLength(0), first.Elements.GetLength(1));
+            for (int i = 0; i < first.Elements.GetLength(0); i++)
+            {
+                for (int j = 0; j < first.Elements.GetLength(1); j++)
+                {
+                    result.Elements[i, j] = first.Elements[i, j] - second.Elements[i, j];
+                }
+            }
+
+            return result;
+        }
+
+        public static Matrix operator *(Matrix first, Matrix second)
+        {
+            if (first.Elements.GetLength(0) != second.Elements.GetLength(1))
+            {
+                throw new ArgumentOutOfRangeException("First matrix should have rows equal to the second matrix'" +
+                "columns in order to perform multiplication.");
+            }
+
+            Matrix result = new Matrix(first.Elements.GetLength(0), second.Elements.GetLength(1));
+
+            for (int i = 0; i < first.Elements.GetLength(0); i++)
+            {
+                for (int j = 0; j < second.Elements.GetLength(1); j++)
+                {
+                    int sum = 0;
+
+                    for (int k = 0; k < second.Elements.GetLength(0); k++)
+                    {
+                        sum += first.Elements[i, k] * second.Elements[k, j];
+                    }
+
+                    result.Elements[i, j] = sum;
+                }
+            }
+
+            return result;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder result = new StringBuilder();
+
+            for (int i = 0; i < this.Elements.GetLength(0); i++)
+            {
+                if (i != 0)
+                {
+                    result.Append("\n");
+                }
+
+                for (int j = 0; j < this.Elements.GetLength(1); j++)
+                {
+                    if (this.Elements[i, j] >= 0 && this.Elements[i, j] < 10)
+                    {
+                        result.AppendFormat("  {0}  ", this.Elements[i, j]);
+                    }
+                    else
+                    {
+                        result.AppendFormat(" {0} ", this.Elements[i, j]);
+                    }
+                }
+            }
+
+            return result.ToString();
+        }
+
+        public override bool Equals(object obj)
+        {
+            bool result = false;
+
+            if (obj == null)
+            {
+                result = false;
+            }
+
+            Matrix second = obj as Matrix;
+            if ((Object)second == null)
+            {
+                result = false;
+            }
+
+            bool equalNumberOfRows = this.Elements.GetLength(0) == second.Elements.GetLength(0);
+            bool equalNumberOfCols = this.Elements.GetLength(1) == second.Elements.GetLength(1);
+            result = (equalNumberOfRows && equalNumberOfCols);
+
+            return result;
+        }
+
+        public override int GetHashCode()
+        {
+            return this.Elements[0, 0];
+        }
+    }
+}
---
+++ Module2/3.DefiningClassesPart2/2.MathMatrix/Test.cs
@@ -0,0 +1,74 @@
+﻿namespace MathMatrix
+{
+    using System;
+    using MathMatrix.Libs;
+
+    public class Test
+    {
+        public static void Main()
+        {
+            Matrix first = new Matrix(3, 3);
+            first.Elements[0, 0] = 2;
+            first.Elements[0, 1] = 25;
+            first.Elements[0, 2] = -5;
+
+            first.Elements[1, 0] = 12;
+            first.Elements[1, 1] = 8;
+            first.Elements[1, 2] = -45;
+
+            first.Elements[2, 0] = 14;
+            first.Elements[2, 1] = -45;
+            first.Elements[2, 2] = 0;
+
+            Matrix second = new Matrix(3, 3);
+            second.Elements[0, 0] = 25;
+            second.Elements[0, 1] = 50;
+            second.Elements[0, 2] = -2;
+
+            second.Elements[1, 0] = 120;
+            second.Elements[1, 1] = -10;
+            second.Elements[1, 2] = -100;
+
+            second.Elements[2, 0] = 32;
+            second.Elements[2, 1] = -101;
+            second.Elements[2, 2] = 98;
+
+            try
+            {
+                Console.WriteLine(first + second);
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.WriteLine();
+
+            try
+            {
+                Console.WriteLine(first - second);
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.WriteLine();
+
+            try
+            {
+                Console.WriteLine(first * second);
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
