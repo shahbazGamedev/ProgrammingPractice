c5d9a71fc2aa9110fadfca6a3d86e988341cb58d
toshkuuu <todorr@live.com>
2015-06-26 13:30:25 +0300

    changes

---
+++ Module2/6.OOPPrinciplesPart2/1.Shapes/Libraries/Rhombus.cs
@@ -0,0 +1,29 @@
+﻿namespace Shapes.Libraries
+{
+    public class Rhombus : Quadrilateral
+    {
+        public Rhombus(double a, double p, double q)
+        {
+            this.SideA = a;
+            this.SideB = a;
+            this.SideC = a;
+            this.SideD = a;
+            this.FirstDiagonal = p;
+            this.SecondDiagonal = q;
+        }
+
+        public double FirstDiagonal { get; set; }
+
+        public double SecondDiagonal { get; set; }
+
+        public override string QuadrilateralSpecificName
+        {
+            get { return "Rhombus"; }
+        }
+
+        protected override double QuadrilateralSpecificSurface
+        {
+            get { return ((this.FirstDiagonal * this.SecondDiagonal) / 2); }
+        }
+    }
+}
---
+++ Module2/6.OOPPrinciplesPart2/1.Shapes/Libraries/Square.cs
@@ -0,0 +1,23 @@
+﻿namespace Shapes.Libraries
+{
+    public class Square : Quadrilateral
+    {
+        public Square(double a)
+        {
+            this.SideA = a;
+            this.SideB = a;
+            this.SideC = a;
+            this.SideD = a;
+        }
+
+        public override string QuadrilateralSpecificName
+        {
+            get { return "Square"; }
+        }
+
+        protected override double QuadrilateralSpecificSurface
+        {
+            get { return (this.SideA * this.SideA); }
+        }
+    }
+}
